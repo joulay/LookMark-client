003d112ac68cde40564aae489c3ba576cd26684c
Julie K <julieskim160@gmail.com>
2018-04-21 18:50:57 -0700

    packages for redux-form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { connect } from 'react-redux';
 import { fetchUsers } from './actions';
@@ -22,16 +21,11 @@ class App extends Component {
   
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+
         <ul>
           {users}
         </ul>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
       </div>
     );
   }
