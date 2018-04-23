db0bb928d4bb2980f930522b44278846802a9702
Julie K <julieskim160@gmail.com>
2018-04-22 20:36:01 -0700

    registration form edit

--- src/components/registration-form.js
+++ src/components/registration-form.js
@@ -1,46 +1,38 @@
 import React from 'react';
-import { Field, reduxForm} from 'redux-form';
+// import { Field, reduxForm} from 'redux-form';
 
-class RegistrationForm extends React.Component {
-    onSubmit(values) {
-        
-    }
-    render() {
-        return (
-            <form className="login-form"> 
-                <label htmlFor="fullName">FULL NAME</label> <br />
-                <Field 
-                    component="input" 
-                    type="text" 
-                    name="fullName" 
-                /> <br />
-                <label htmlFor="username">USERNAME</label> <br />
-                <Field 
-                    component="input" 
-                    type="text" 
-                    name="username" 
-                /> <br />
-                <label htmlFor="email">EMAIL</label> <br />
-                <Field 
-                    component="input" 
-                    type="text" 
-                    name="email" 
-                /> <br />
-                <label htmlFor="password">PASSWORD</label> <br />
-                <Field 
-                    component="input" 
-                    type="text" 
-                    name="password" 
-                /> <br />
-                <button
-                    type="submit">
-                    CREATE AN ACCOUNT 
-                </button>
-            </form>
+export default function RegistrationForm(){
+    return (
+        <form className="login-form"> 
+            
+            <label htmlFor="fullName">FULL NAME</label> <br />
+            <input 
+                type="text" 
+                name="fullName" 
+            /> <br />
+            <label htmlFor="email">EMAIL</label> <br />
+            <input 
+                type="text" 
+                name="email" 
+            /> <br />
+            <label htmlFor="username">USERNAME</label> <br />
+            <input 
+                type="text" 
+                name="username" 
+            /> <br />
+            <label htmlFor="password">PASSWORD</label> <br />
+            <input 
+                type="text" 
+                name="password" 
+            /> <br />
+            <button
+                type="submit">
+                CREATE AN ACCOUNT 
+            </button>
+        </form>
         )
-    }
-}  
+    }  
 
-export default reduxForm({
-    form: 'registerUser'
-})(RegistrationForm);  
+// export default reduxForm({
+//     form: 'registerUser'
+// })(RegistrationForm);  
