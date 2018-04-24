3e67957b5de02e64f431fe39ea5a50de75e57c7b
Julie K <julieskim160@gmail.com>
2018-04-23 21:52:38 -0700

    registering

--- src/components/registration-form.js
+++ src/components/registration-form.js
@@ -3,7 +3,8 @@ import { Link } from 'react-router-dom';
 // import { combineUser } from '../reducers/user';
 import { Field, reduxForm} from 'redux-form';
 import  { createNewUser } from '../actions/createUser';
-// import { login } from '../actions/login';
+import Input from './input';
+import { login } from '../actions/auth';
 
 class RegistrationForm extends React.Component{
     onSubmit(values) {
@@ -11,7 +12,7 @@ class RegistrationForm extends React.Component{
         const user = {fullName, email, username, password};
         return this.props
             .dispatch(createNewUser(user))
-            // .then(()=>this.props.dispatch(login(username, password)))
+            .then(()=>this.props.dispatch(login(username, password)))
     }
     render() {
     return (
@@ -23,22 +24,22 @@ class RegistrationForm extends React.Component{
                 onSubmit={this.props.handleSubmit(values =>
                     this.onSubmit(values)) }> 
                 <label htmlFor="fullName">FULL NAME</label> <br />
-                <Field component="input" 
+                <Field component={Input}
                     type="text" 
                     name="fullName" 
                 /> <br />
                 <label htmlFor="email">EMAIL</label> <br />
-                <Field component="input" 
+                <Field component={Input}
                     type="text" 
                     name="email" 
                 /> <br />
                 <label htmlFor="username">USERNAME</label> <br />
-                <Field component="input" 
+                <Field component={Input}
                     type="text" 
                     name="username" 
                 /> <br />
                 <label htmlFor="password">PASSWORD</label> <br />
-                <Field component="input" 
+                <Field component={Input}
                     type="text" 
                     name="password" 
                 /> <br />
--- src/store.js
+++ src/store.js
@@ -1,11 +1,12 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { combineUser } from './reducers/user';
+import { reducer as formReducer } from 'redux-form';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 const store = createStore(
-    combineReducers({user:combineUser
-     
+    combineReducers({user:combineUser,
+    form: formReducer,
 
 }), 
 composeWithDevTools(applyMiddleware(thunk)))
