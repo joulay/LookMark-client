c77b45ffb9e3fec8df73d438766a36176c40f3dc
Julie K <julieskim160@gmail.com>
2018-04-25 08:21:27 -0700

    add auth router to store

--- src/store.js
+++ src/store.js
@@ -2,12 +2,13 @@ import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { combineUser } from './reducers/user';
 import { reducer as formReducer } from 'redux-form';
 import thunk from 'redux-thunk';
+import authReducer from './reducers/auth';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 const store = createStore(
     combineReducers({user:combineUser,
     form: formReducer,
-
+    auth: authReducer,
 }), 
 composeWithDevTools(applyMiddleware(thunk)))
 
