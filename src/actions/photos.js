6ba0950c58e20953729cd6feb91c7a827f3a3bb2
Julie K <julieskim160@gmail.com>
2018-05-01 17:35:49 -0700

    post photo action

--- src/actions/photos.js
+++ src/actions/photos.js
@@ -1,16 +1,44 @@
 import { API_BASE_URL } from '../config';
 
 
-const fetchPhotoSuccess = (users) => ({
-    type: 'FETCH_PHOTOS_SUCCESS',
-    users
+export const GET_PHOTO_SUCCESS = 'GET_PHOTO_SUCCESS';
+export const getPhotoSuccess = photos => ({
+    type: GET_PHOTO_SUCCESS,
+    photos
 })
 
-export const fetchPhoto = () => {
-    return (dispatch) => {
-        fetch(`${API_BASE_URL}/api/getphotos/`)
-        .then(response => response.json())
-        .then(photo => dispatch(fetchPhotoSuccess(photo)))
-        .catch(err => console.log(err))
-    }
-}
+
+export const postPhoto = (photo) => dispatch => {
+    const authToken = localStorage.getItem('authToken');
+    const brideId = localStorage.getItem('brideId');
+    fetch(`${API_BASE_URL}/api/photos/${brideId}`, {
+        method: 'POST',
+        header: {
+            Authorization: `Bearer ${authToken}`
+        },
+        body: photo
+    })
+        .then(res => {
+            if(!res.ok) {
+                return Promise.reject(res.statusText);
+            }
+            return res.json();
+        })
+        .then(response => {
+            dispatch(getPhotoSuccess(response.photos));
+        });
+};
+
+// const fetchPhotoSuccess = (users) => ({
+//     type: 'FETCH_PHOTOS_SUCCESS',
+//     users
+// })
+
+// export const fetchPhoto = () => {
+//     return (dispatch) => {
+//         fetch(`${API_BASE_URL}/api/getphotos/`)
+//         .then(response => response.json())
+//         .then(photo => dispatch(fetchPhotoSuccess(photo)))
+//         .catch(err => console.log(err))
+//     }
+// }
