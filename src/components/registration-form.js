5db00b80de126bac48e25bf75010a8c13ce92a2e
Julie K <julieskim160@gmail.com>
2018-05-02 22:26:02 -0700

    background for register

--- src/components/registration-form.js
+++ src/components/registration-form.js
@@ -4,7 +4,14 @@ import { Link } from 'react-router-dom';
 import { Field, reduxForm } from 'redux-form';
 import  { newUser } from '../actions/createUser';
 import Input from './input';
+import background from '../decor/blackmarble.jpg';
 import './registration-form.css';
+
+const sectionStyle = {
+    width: "100%",
+    height: "100VH",
+    backgroundImage: `url(${background})`
+  };
 
 
 class RegistrationForm extends React.Component{
@@ -27,49 +34,51 @@ class RegistrationForm extends React.Component{
     render() {
         
     return (
-        <div className="register-page">
-            <Link to="/signup">Back</Link>
-            <p>{this.state.msg ? this.state.msg : ''}</p>
-            <form 
-                className="form" 
-                onSubmit={this.props.handleSubmit(values =>
-                    this.onSubmit(values)) }> 
+        <section style={ sectionStyle }>
+            <div className="register-page">
+                <Link className="back-link" to="/signup">Back</Link>
+                <p>{this.state.msg ? this.state.msg : ''}</p>
+                <form 
+                    className="form" 
+                    onSubmit={this.props.handleSubmit(values =>
+                        this.onSubmit(values)) }> 
 
-                <Field component={Input}
-                    type="text" 
-                    name="fullName" 
-                    placeholder="FULL NAME"
-                /> <br />
+                    <Field component={Input}
+                        type="text" 
+                        name="fullName" 
+                        placeholder="FULL NAME"
+                    /> <br />
 
-                <Field component={Input}
-                    type="email" 
-                    name="email"  
-                    placeholder="EMAIL"
-                /> <br />
+                    <Field component={Input}
+                        type="email" 
+                        name="email"  
+                        placeholder="EMAIL"
+                    /> <br />
 
-                <Field component={Input}
-                    type="text" 
-                    name="username" 
-                    placeholder="USERNAME"
-                /> <br />
+                    <Field component={Input}
+                        type="text" 
+                        name="username" 
+                        placeholder="USERNAME"
+                    /> <br />
 
-                <Field component={Input}
-                    type="text" 
-                    name="password" 
-                    placeholder="PASSWORD"
-                /> <br />
+                    <Field component={Input}
+                        type="text" 
+                        name="password" 
+                        placeholder="PASSWORD"
+                    /> <br />
 
-                <button
-                    className="form-button"
-                    type="submit">
-                    REGISTER 
-                </button>
+                    <button
+                        className="form-button"
+                        type="submit">
+                        REGISTER 
+                    </button>
 
-                <br />
+                    <br />
 
-            </form>
-            
-        </div>
+                </form>
+                
+            </div>
+        </section>
         )
     }  
 }
