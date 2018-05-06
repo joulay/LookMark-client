2bee75f24cc1e72c85f90b4808bb1630aa2f754b
Elias Mason <eliasjmason@gmail.com>
2018-05-06 12:50:40 -0700

    cleanup

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,15 @@
 import { API_BASE_URL } from '../config';
-// const API_BASE_URL = 'https://lookmarkapp.herokuapp.com/api'
 
-const fetchUsersSuccess = (users) => ({
-    type: 'FETCH_USERS_SUCCESS',
-    users
-})
+const fetchUsersSuccess = users => ({
+  type: 'FETCH_USERS_SUCCESS',
+  users
+});
 
 export const fetchUsers = () => {
-    return (dispatch) => {
-        fetch(`${API_BASE_URL}/api/users`)
-        .then(response => response.json())
-        .then(users => dispatch(fetchUsersSuccess(users)))
-        .catch(err => console.log(err))
-    }
-}
-
+  return dispatch => {
+    fetch(`${API_BASE_URL}/api/users`)
+      .then(response => response.json())
+      .then(users => dispatch(fetchUsersSuccess(users)))
+      .catch(err => console.log(err));
+  };
+};
