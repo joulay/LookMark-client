0408774a5229e4b1114b179273dfea3795f1ad09
Julie K <julieskim160@gmail.com>
2018-07-04 16:37:15 -0700

    oconfig and env

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
+
 export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api'
+
 
 
 export const PHOTO_BASE_URL = process.env.REACT_APP_PHOTO_BASE_URL || 'http://localhost:8080'
