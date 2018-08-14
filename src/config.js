f34f18a564de42eb3c1ac02afa81b530c8c43d6f
Julie K <julieskim160@gmail.com>
2018-08-14 15:57:10 -0700

    index.html

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api'
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL === 'production'
+ ? 'https://lookmarkapp.herokuapp.com/api' 
+ : 'http://localhost:8080/api'
 
 
 
