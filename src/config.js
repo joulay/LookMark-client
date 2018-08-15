dfef22affb7e7c2956e84086523a3b7d16348908
Julie K <julieskim160@gmail.com>
2018-08-14 17:28:38 -0700

    cors bug

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL === 'production'
+export const API_BASE_URL = process.env.NODE_ENV === 'production'
  ? 'https://lookmarkapp.herokuapp.com/api' 
  : 'http://localhost:8080/api'
 
